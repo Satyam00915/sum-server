cbe8af79b2a6655cee13eb85000f57572721e6b4
Satyam00915 <satyamupadhyay00915@gmail.com>
2024-07-18 22:53:19 +0530

    backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  const a = req.query.a;
+  const b = req.query.b;
+  res.json({
+    sum: `The sum is: ${parseInt(a) + parseInt(b)}`,
+  });
+});
+
+app.listen(5000);
